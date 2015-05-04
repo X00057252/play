ef54cbb115e4960dcbbe699daaa871dba7194062
x00107883 <X00107883@gmail.com>
2015-05-04 10:07:30 +0100

    Code visibility fixes
    - re-organised CSS file, added more comments there
    - some comments added in different views
    - removed modal pop up functions from javascript - we don't use them

--- public/javascripts/progressSchool.js
+++ public/javascripts/progressSchool.js
@@ -6,12 +6,15 @@
 	April 2015 
 
 =========================================================== */
+
+
+
 /* Governing read more/less links in articles: --
  		shID+'show' - "Read more" link
 		shID - Hidden section          */  
-
+		
 function showHide(shID) {
-	var top = document.getElementById(shID+'-top').offsetTop; //Getting Y of top of article
+	var top = document.getElementById(shID+'-top').offsetTop; //Getting position of top of article
 	if (document.getElementById(shID)) {
 		//if Read More link visible - hide Read More link, display hidden section:
 		if (document.getElementById(shID+'-show').style.display != 'none') {
@@ -32,78 +35,6 @@ function showHide(shID) {
 
 
 
-
-// Opens a modal pop up window: --
-
-function popUp() { 
-    this.confBox = null;   // confBox - confirmation message box
-    this.confBack = null;  // confBack - transparent grey background 
-
-	// this creates pop-up window message elements and opens pop up:
-    this.popOut = function() {
-        //create grey transparent background:
-        this.confBack = document.createElement("div");
-        this.confBack.className = "confBack";
-		// create message box:
-        this.confBox = document.createElement("div");
-        this.confBox.className = "confBox";
-        this.confBox.Code = this;
-		//create message itself:
-        var msg = document.createElement("article");
-        msg.className = "confMsg";
-        msg.innerHTML = 'Thank you!<br>The form has been submitted.<br><br>Click "Close" to go back to home page.';
-        this.confBox.appendChild(msg);
-        //create button to close the pop-up:
-		var closebtn = document.createElement("button");
-		closebtn.className="popBttn";
-        closebtn.onclick = function() { 
-            this.parentNode.Code.popIn();
-        }
-        closebtn.innerHTML = "Close";
-        this.confBox.appendChild(closebtn);
-
-        document.body.appendChild(this.confBack);
-        document.body.appendChild(this.confBox);
-    }
-
-
-	// this closes pop up window: --
-    this.popIn = function() {
-        // If confBox is displayed (window open) - close it
-		if (this.confBox != null) {
-            document.body.removeChild(this.confBox);
-            this.confBox = null;
-        }
-		// If confBack is displayed (window open) - close it
-        if (this.confBack != null) {
-			document.body.removeChild(this.confBack);
-        this.confBack = null;
-        }
-		location.href = "index.html";
-	} 
-}
-// End of: opens a modal pop up window --
-
-
-
-
-
-
-
-//creates new popUp window object:
-
-function callPop(){
-	var pop = new popUp();
-    pop.popOut();
-	return false;
-}
-// End of: creates new popUp window --
-
-
-
-
-
-
 // Check if email and confirmed email are the same: --
 
 function emailCheck(input) {
@@ -117,6 +48,10 @@ function emailCheck(input) {
 	}
 }
 // End of: Check if email and confirmed email are the same --
+
+
+
+
     $(function() {
         if($('#selectedCourseId').val())
         {
